103504af8af42566722c259449f561e3ab4fb39c
Alex Lee <525210804@qq.com>
2017-09-02 17:10:38 +0800

    Create data_preload.py
    Added data preload function for the module

---
+++ data_preload.py
@@ -0,0 +1,11 @@
+import os
+import pickle
+from pathlib import Path
+pickle_file = Path("env_config.pickle")
+
+if pickle_file.exists():
+    data = pickle.load(open(pickle_file, "rb"))
+    #print(data)
+
+os.chdir(os.path.join(data.get("PRJ_HOME_PATH"), data.get("DATA_TICKER_DOWNLOAD_PATH")))
+os.system("./TickerDownloader.py stocks")
